b703de50bb2a24ee417f723b739b059a98b4269b
Jesse Niininen <findarkside@gmail.com>
2019-11-16 18:41:48 +0200

    Implement User model

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,6 @@ const db = knex(knexFile.development);
 export default db
 
 export async function resetDb() {
-    await db.schema.dropTableIfExists('password');
     await db.schema.dropTableIfExists('user');
 
     await db.schema.createTable('user', (table) => {
@@ -14,12 +13,8 @@ export async function resetDb() {
         table.string('username').unique().notNullable();
         table.specificType('created_at', 'TIMESTAMP WITH TIME ZONE');
         table.string('email');
+        table.string('password_hash').notNullable();
+        table.string('hash_type').notNullable();
     });
 
-    await db.schema.createTable('password', (table) => {
-        table.integer('user_id').primary();
-        table.string('hash').notNullable();
-        table.string('hash_type').notNullable();
-        table.foreign("user_id").references("user.id");
-    });
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,34 @@
 import db from '../db';
+import uuid from 'uuid';
 
 export interface User {
-    pk: number;
-    id: string;
+    id: number;
+    uid: string;
+    createdAt: Date;
     username: string;
     email: string;
+    passwordHash: string;
+    hashType: string;
+}
+type UserProjection = {
+    [key in keyof User]: 0 | 1;
 }
 
+export async function create(user: Omit<User, 'id' | 'uid' | 'createdAt'>) {
+    const newUser: Omit<User, 'id'> = {
+        uid: uuid.v4(),
+        createdAt: new Date(),
+        ...user,
+    };
+
+    const res = await db('user').insert(newUser);
+    return newUser;
+}
+
+export async function findOne(filter: Partial<User>, projection: UserProjection) {
+    const keys = Object.keys(projection).filter(key => projection[key] === 1);
+    db
+        .select(keys)
+        .where(filter)
+        .from('user');
+}
