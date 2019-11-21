fcf70f5bd6bd6af1f28747d875e823807bf002d4
Jesse Niininen <findarkside@gmail.com>
2019-11-21 22:23:07 +0200

    Reset config between tests

--- test/_index.ts
+++ test/_index.ts
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import config from '../src/config';
 import db, { resetDb } from '../src/db';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
@@ -9,10 +10,21 @@ before(async function () {
     console.log('asd');
     this.timeout(10000);
     await db.raw('SELECT 0');
-})
+});
+
+const defaultConfig: Partial<typeof config> = {
+    bcryptWorkFactor: 4,
+    defaultSessionLength: 1000 * 60 * 60 * 24,
+    sessionTokenBytes: 16,
+    user: {
+        minPasswordLength: 8,
+        minUsernameLength: 3,
+    }
+}
 
 beforeEach(async function () {
     await resetDb();
+    Object.assign(config, defaultConfig);
 });
 
 const expect = chai.expect;
