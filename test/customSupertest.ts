a1143fd41c94c78e88704a9a5d59259034e3b4f0
Jesse <jesseniininen@gmail.com>
2020-12-30 13:31:05 +0200

    Update supertest, fix custom supertest expect function

--- test/customSupertest.ts
+++ test/customSupertest.ts
@@ -6,11 +6,12 @@ import status from 'statuses';
     this: supertest.Test,
     statusCode: number
 ) {
-    this.then((res) => {
+    return this.then((res) => {
         if (res.status !== statusCode) {
-            const message = `expected ${statusCode} "${status(statusCode)}", got ${res.status} "${status(res.status)}" response: ${JSON.stringify(res.body, null, 2)}`;
+            const message = `expected ${statusCode} "${status(statusCode)}", got ${res.status} "${status(res.status)}" 
+            response: ${JSON.stringify(res.body, null, 2)}`;
             throw message;
         }
+        return this;
     });
-    return this;
 };
