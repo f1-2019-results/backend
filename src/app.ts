698b512196dbd0725563d93c4d25fb354e490e12
Jesse <jesseniininen@gmail.com>
2021-03-27 14:10:30 +0200

    Enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import authRouter from './routes/auth';
 import raceRouter from './routes/race';
 import bodyParser from 'body-parser';
 import { errorHandler } from './util/middleware';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/auth', authRouter);
 app.use('/race', raceRouter);
 app.get('/', (req, res) => {
     res.json('Hello world');
 });
+
 app.use(errorHandler);
 
 export default app;
