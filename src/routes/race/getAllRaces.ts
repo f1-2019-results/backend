f800c4a4234bfebe9dee536d4e5e32e597dff271
Jesse <jesseniininen@gmail.com>
2021-04-05 20:01:11 +0300

    Remove console logs, fix lint errors

--- src/routes/race/getAllRaces.ts
+++ src/routes/race/getAllRaces.ts
@@ -4,10 +4,8 @@ import asyncRequestHandler from 'util/asyncRequestHandler';
 import db from 'db';
 
 export default asyncRequestHandler(async (req: Request, res: Response) => {
-    console.log('asd')
     const races = await db.races.find({
         relations: ['track']
     });
-    console.log(races)
     return res.json(races);
 });
